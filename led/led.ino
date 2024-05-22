0970219f41351fdc20371111673b20042c6b0d8a
Pasan Wijekoon <pasan.16109@gmail.com>
2024-05-22 22:43:20 +0530

    Add led.ino file

---
+++ led/led.ino
@@ -0,0 +1,89 @@
+int LED_1 = 13;
+int LED_2 = 12;
+int LED_3 = 11;
+int LED_4 = 10;
+int LED_5 = 9;
+int LED_6 = 8;
+int LED_7 = 7;
+int LED_8 = 6;
+int LED_9 = 5;
+int LED_10 = 4;
+int Delay = 1000;
+
+void setup() {
+  pinMode(LED_1, OUTPUT);
+  pinMode(LED_2, OUTPUT);
+  pinMode(LED_3, OUTPUT);
+  pinMode(LED_4, OUTPUT);
+  pinMode(LED_5, OUTPUT);
+  pinMode(LED_6, OUTPUT);
+  pinMode(LED_7, OUTPUT);
+  pinMode(LED_8, OUTPUT);
+  pinMode(LED_9, OUTPUT);
+  pinMode(LED_10, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(LED_1, HIGH);   
+  delay(Delay);                       
+  digitalWrite(LED_1, LOW);     
+  digitalWrite(LED_2, HIGH);   
+  delay(Delay);                       
+  digitalWrite(LED_2, LOW);     
+  digitalWrite(LED_3, HIGH);   
+  delay(Delay);                       
+  digitalWrite(LED_3, LOW);     
+  digitalWrite(LED_4, HIGH);   
+  delay(Delay);                       
+  digitalWrite(LED_4, LOW);     
+  digitalWrite(LED_5, HIGH);   
+  delay(Delay);                       
+  digitalWrite(LED_5, LOW);     
+  digitalWrite(LED_6, HIGH);   
+  delay(Delay);                       
+  digitalWrite(LED_6, LOW);     
+  digitalWrite(LED_7, HIGH);   
+  delay(Delay);                       
+  digitalWrite(LED_7, LOW);    
+  digitalWrite(LED_8, HIGH);   
+  delay(Delay);                       
+  digitalWrite(LED_8, LOW);    
+  digitalWrite(LED_9, HIGH);   
+  delay(Delay);                       
+  digitalWrite(LED_9, LOW);     
+  digitalWrite(LED_10, HIGH);   
+  delay(Delay);                       
+  digitalWrite(LED_10, LOW);   
+  digitalWrite(LED_10, HIGH);   
+  delay(Delay);                       
+  digitalWrite(LED_10, LOW);     
+  digitalWrite(LED_9, HIGH);   
+  delay(Delay);                       
+  digitalWrite(LED_9, LOW);     
+  digitalWrite(LED_8, HIGH);   
+  delay(Delay);                       
+  digitalWrite(LED_8, LOW);     
+  digitalWrite(LED_7, HIGH);   
+  delay(Delay);                       
+  digitalWrite(LED_7, LOW);     
+  digitalWrite(LED_6, HIGH);   
+  delay(Delay);                       
+  digitalWrite(LED_6, LOW);     
+  digitalWrite(LED_5, HIGH);   
+  delay(Delay);                       
+  digitalWrite(LED_5, LOW);     
+  digitalWrite(LED_4, HIGH);   
+  delay(Delay);                       
+  digitalWrite(LED_4, LOW);    
+  digitalWrite(LED_3, HIGH);   
+  delay(Delay);                       
+  digitalWrite(LED_3, LOW);    
+  digitalWrite(LED_2, HIGH);   
+  delay(Delay);                       
+  digitalWrite(LED_2, LOW);     
+  digitalWrite(LED_1, HIGH);   
+  delay(Delay);                       
+  digitalWrite(LED_1, LOW);    
+  
+}
